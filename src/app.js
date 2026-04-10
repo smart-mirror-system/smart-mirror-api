8c88e1cfab0c63744b85e1964541788577f83d87
Mohamed Fouad <mohamed.fouad.softwareengineer@gmail.com>
2026-04-10 18:44:41 +0200

    feat: implement rate limiting for API requests and add environment variables documentation

--- src/app.js
+++ src/app.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-
+const ratelimit = require('express-rate-limit')
 const healthRoutes = require('./routes/health.routes');
 const authRoutes = require('./routes/auth.routes');
 const meRoutes = require('./routes/me.routes');
 const sessionsRoutes = require('./routes/sessions.routes');
 const analyticsRoutes = require('./routes/analytics.routes');
 const { notFound, errorHandler } = require('./middleware/error');
+const { loadEnv } = require('./config/env');
+
+loadEnv();
 
 const app = express();
+const globalLimiter = ratelimit({
+  windowMs: 15 * 60 * 1000,
+  max: Number(process.env.REQUEST_LIMIT) || 100,
+  message: { 
+    ok: false, 
+    error: 'Too many requests from this IP, please try again after 15 minutes' 
+  },
+});
 
 app.use(cors());
-app.use(express.json());
 app.use(morgan('dev'));
+app.use(globalLimiter);
+app.use(express.json());
 /**
  * Health and readiness routes.
  * Exposed for Kubernetes probes and monitoring.
--- src/routes/me.routes.js
+++ src/routes/me.routes.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const rateLimit = require('express-rate-limit');
+
 const auth = require('../middleware/auth');
 const {
   me,
@@ -7,12 +9,22 @@ const {
   deleteTrainingSchedule,
 } = require('../controllers/me.controller');
 
+const generateScheduleLimit = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 5,
+  message: { 
+    ok: false, 
+    error: 'Too many requests from this IP, please try again later after 1 hour from the last request' 
+  },
+});
+
+
 router.get('/', auth, me);
 router
   .route('/schedule')
   .get(auth, getTrainingSchedule)
-  .post(auth, createTrainingSchedule)
-  .put(auth, createTrainingSchedule)
+  .post(auth, generateScheduleLimit, createTrainingSchedule)
+  .put(auth, generateScheduleLimit, createTrainingSchedule)
   .delete(auth, deleteTrainingSchedule);
 
 module.exports = router;
