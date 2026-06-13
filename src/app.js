85d34066b2caaa23f4cf6a44a6b1f28689062cdd
Mohamed Fouad <mohamed.fouad.softwareengineer@gmail.com>
2026-06-13 08:10:56 +0300

    feat: replace deprecated xss-clean with express-xss-sanitizer; update JSON body limit

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const xss = require('xss-clean');
+const { xss } = require('express-xss-sanitizer');
 const healthRoutes = require('./routes/health.routes');
 const authRoutes = require('./routes/auth.routes');
 const meRoutes = require('./routes/me.routes');
@@ -24,7 +24,7 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan('dev'));
 app.use(globalLimiter);
-app.use(express.json());
+app.use(express.json({ limit: '50kb' }));
 app.use(xss());
 
 app.use('/', healthRoutes);
